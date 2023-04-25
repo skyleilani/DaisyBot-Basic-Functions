05bacea558215a27e304593e1f24ca65e5148422
sky leilani <45158875+skyleilani@users.noreply.github.com>
2023-04-25 08:56:33 -0700

    Update DaisyBot_Motor_Control.ino

--- DaisyBot_Motor_Control.ino
+++ DaisyBot_Motor_Control.ino
@@ -1,9 +1,19 @@
 /*
- this script is for testing the connection between the TB6612 driver, Arduino Mega, and two Dagu DC motors only
-*/ 
+ * Daisy Bot Basic Wiring Script
+ * Author: Sky Leilani
+ *
+ * This script provides basic wiring and motor control for Dr. Carlotta Berry's Daisy Bot.
+ * It defines pin configurations for the Arduino Mega and TB6612 motor driver, sets motor directions and speed, 
+ *  and includes utility functions for pivoting and stopping the robot. 
+ *
+ * GitHub Repository: https://github.com/skyleilani/DaisyBot-Basic-Functions
+ */
 
+// Offsets are used to adjust speed of each motor in the case of speed differences between wheels 
+const int offsetA = 0; 
+const int offsetB = 0; 
 
-// pins for inputs, PWM defines must be on PWM pins 
+// Pins for inputs, PWM defines must be on PWM pins 
 #define AIN1 2
 #define BIN1 6
 #define AIN2 4
@@ -11,116 +21,120 @@
 #define PWMA 3
 #define PWMB 5
 
-// define motor directions 
+// Motor directions 
 #define FORWARD 1 
 #define BACKWARD 2 
 #define LEFT 3 
 #define RIGHT 4 
 #define STOP 5 
 
-/* 
-standby which is optional you can run this for errors program it later maybee  
-#define STBY 9 
-*/
-
-// Safeguard against wiring differences or future changes
-const int offsetA = 1; 
-const int offsetB = -1; 
-
-// Set motor direction and speed 
+// Set motor direction and speed functionality for each motor individually
 void setMotor(int motor, int direction, int speed) { 
-  // set motor direction pins based on the input direction 
-
+   
+  // Motor A functionality (left motor)
   if(motor == 1) { 
     if (direction == FORWARD){ 
       digitalWrite(AIN1, LOW); 
       digitalWrite(AIN2, HIGH); 
     } 
-    
     else if (direction == BACKWARD) { 
       digitalWrite(AIN1, HIGH); 
       digitalWrite(AIN2, LOW); 
     }
-
     else if (direction == STOP) { 
       digitalWrite(AIN1, LOW); 
       digitalWrite(AIN2, LOW); 
     }
-
-    analogWrite(PWMA, speed+offsetA);  
+    if (speed >= 0 && speed <= 255){
+       analogWrite(PWMA, speed+offsetA);  
+    } else {
+      Serial.println("Err: Please choose a speed value between 0 and 255.");  
+    }      
   }
 
+  // motor B functionality (right motor) 
    else if(motor == 2) { 
     if (direction == FORWARD) { 
       digitalWrite(BIN1, LOW); 
       digitalWrite(BIN2, HIGH); 
     }
-
     else if (direction == BACKWARD) { 
       digitalWrite(BIN1, HIGH); 
       digitalWrite(BIN2, LOW);  
     }
-
     else if (direction == STOP) { 
       digitalWrite(BIN1, LOW); 
       digitalWrite(BIN2, LOW); 
     }
-
-    analogWrite(PWMB, speed+offsetB);  // ensure motors run at the same speed 
-   }
+    if (speed >= 0 && speed <= 255){
+       analogWrite(PWMB, speed+offsetB);  
+    } else {
+      Serial.println("Err: Please choose a speed value between 0 and 255.");    
+    }
+  }
 }
 
-// Initialize motor control pinss as outputs !
+// Stop both motors 
+void stopRobot() {
+  setMotor(1, STOP, 0);
+  setMotor(2, STOP, 0);
+}
+
+// Pivot right by running the left motor and not the right 
+void pivotRight(int speed) {
+  setMotor(1, FORWARD, speed);
+  setMotor(2, STOP, 0);
+}
+
+// Pivot left by running right motor and not the left 
+void pivotLeft(int speed) {
+  setMotor(1, STOP, 0);
+  setMotor(2, FORWARD, speed);
+}
+
+// Run both motors forward 
+void forward(int speed) {
+  setMotor(1, FORWARD, speed);
+  setMotor(2, FORWARD, speed);
+}
+
+// Run both motors backward 
+void backward(int speed) {
+  setMotor(1, BACKWARD, speed);
+  setMotor(2, BACKWARD, speed);
+}
+
+// Initialize motor control pins as outputs 
 void setup() {
+  // motor A 
   pinMode(AIN1, OUTPUT); 
   pinMode(AIN2, OUTPUT); 
-  
+
+  // motor B 
   pinMode(BIN1, OUTPUT); 
   pinMode(BIN2, OUTPUT); 
 
+  // speed for motor A & motor B 
   pinMode(PWMA, OUTPUT); 
   pinMode(PWMB, OUTPUT);  
 }
 
-// tests basic functionality repeatedly
+// Repeat function testing 
 void loop() {
-  // move robot forward at half speed for 2 seconds 
-  setMotor(1, FORWARD, 128);
-  setMotor(2, FORWARD, 128); 
+  
+  forward(128);
   delay(2000); 
-
-  //stop robot for 1 second
-  setMotor(1, STOP, 0); 
-  setMotor(2, STOP, 0); 
-  delay(1000); 
-
-  // pivot the robot left at half speed for 2 seconds 
-  setMotor(1, FORWARD, 128); 
-  setMotor(2, BACKWARD, 128); 
+  stopRobot();
   delay(2000);
+  
+  pivotLeft(128); 
+  delay(2000); 
+  stopRobot();
+  delay(2000); 
+  
+  pivotRight(128); 
+  delay(2000); 
+  stopRobot();
+  delay(2000); 
    
-  // stop the robot for 1 second
-  setMotor(1, STOP, 0);
-  setMotor(2, STOP, 0);
-  delay(1000);
-  
-  // move the robot backward at half speed for 2 seconds
-  setMotor(1, BACKWARD, 128);
-  setMotor(2, BACKWARD, 128);
-  delay(2000);
-  
-  // stop the robot for 1 second
-  setMotor(1, STOP, 0);
-  setMotor(2, STOP, 0);
-  delay(1000);
-  
-  // pivot the robot right at half speed for 2 seconds
-  setMotor(1, BACKWARD, 128);
-  setMotor(2, FORWARD, 128);
-  delay(2000);
-
-  // stop the robot for 1 second
-  setMotor(1, STOP, 0);
-  setMotor(2, STOP, 0);
-  delay(1000);
 }
